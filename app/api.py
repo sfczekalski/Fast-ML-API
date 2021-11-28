fd43e4aebd1b64a2dc3aff8930a5611f2986d73d
Stanislaw Czekalski <sfczekalski@gmail.com>
2021-11-28 14:42:00 +0100

    Call predict in api.

--- app/api.py
+++ app/api.py
@@ -6,6 +6,8 @@ from typing import Any, Callable, Dict, Union
 
 from fastapi import FastAPI, Request, status
 
+from eloquent_matsumoto import predict as predict_module
+
 app = FastAPI(
     title="Model serving API",
     description="Model serving API developed using FastAPI framework",
@@ -61,9 +63,35 @@ def health(request: Request) -> Dict[str, Union[str, Any]]:
         Dict[str, Union[str, Any]]: Health check response.
     """
     response = {
-        "message": "Healthy",
+        "message": "OK",
         "status-code": status.HTTP_200_OK,
         "data": {},
+    }
+
+    return response
+
+
+@app.post("/predict/")
+@add_response_metadata
+def predict(request: Request, payload: Any) -> Dict[str, Union[str, Any]]:
+    """Predict endpoint.
+
+    Args:
+        request (Request): The request object.
+
+    Returns:
+        Dict[str, Union[str, Any]]: Prediction response.
+    """
+    texts = [item.text for item in payload["texts"]]
+
+    predictions = predict_module._predict(texts)
+
+    response = {
+        "message": "OK",
+        "status-code": status.HTTP_200_OK,
+        "data": {
+            "predictions": predictions,
+        },
     }
 
     return response
