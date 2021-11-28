87052ee437a8990de3a3d0e80b0d3fd76b9197fb
Stanislaw Czekalski <sfczekalski@gmail.com>
2021-11-28 14:27:28 +0100

    Package subdirectory under src.

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="eloquent_matsumoto",
     version="0.1",
     description="My ML package",
-    packages=["src"],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
 )
