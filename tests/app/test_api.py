7095b33b8b0cad667079edbe49ba88d6b94aa4f0
Stanislaw Czekalski <sfczekalski@gmail.com>
2021-12-03 16:58:42 +0100

    Tests for predict.

--- tests/app/test_api.py
+++ tests/app/test_api.py
@@ -28,3 +28,37 @@ def test_health(client: TestClient):
     assert response["message"] == "OK"
     assert response["status-code"] == 200
     assert response["data"] == {}
+
+
+def test_predict(client: TestClient):
+    payload = {
+        "texts": [
+            {"text": "Examplary text"},
+            {"text": "Another one"},
+        ]
+    }
+
+    response = client.post(
+        url="/predict",
+        json=payload,
+    )
+    assert response.status_code == 200
+
+    response = response.json()
+    assert response["message"] == "OK"
+    assert response["status-code"] == 200
+
+
+def test_predict_no_texts(client: TestClient):
+    payload = {"texts": []}
+
+    response = client.post(
+        url="/predict",
+        json=payload,
+    )
+
+    assert response.status_code == 422
+
+    response = response.json()
+    assert response["detail"][0]["type"] == "value_error"
+    assert response["detail"][0]["msg"] == "Texts list cannot be empty"
